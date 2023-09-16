f337687010f661fecb62b850ea00b9af567cecf5
muzammil <abc@xyz.com>
2023-09-16 17:44:42 +0530

    ui & logic of code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import { Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import Body from "./pages/Body";
+import Header from "./components/Header";
+import store from "./utils/store";
+import WatchPage from "./pages/WatchPage";
+import ResultsPage from "./pages/ResultsPage";
+import { Profile } from "./pages/Profile";
+import { Explore } from "./pages/ExplorePages";
+import Liked from "./pages/Liked";
+import WatchLater from "./pages/WatchLater";
+import History from "./pages/History";
+import { Toaster } from "react-hot-toast";
+
 function App() {
-  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <Provider store={store}>
+      <div className="App relative ">
+        <Toaster position="top-center" reverseOrder={false} />
+        <Header />
+        <Routes>
+          <Route path="/" element={<Body />} />
+          <Route path="/watch" element={<WatchPage />} />
+          <Route path="/results" element={<ResultsPage />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/likedvideos" element={<Liked />} />
+          <Route path="/watchlater" element={<WatchLater />} />
+          <Route path="/history" element={<History />} />
+        </Routes>
+      </div>
+    </Provider>
+  );
 }
 
 export default App;
